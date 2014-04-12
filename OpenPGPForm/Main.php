3a10869407b55c8818a91b31eacd55ea036d0163
Marcus Povey <marcus@marcus-povey.co.uk>
2014-04-12 14:39:24 +0100

    Decrypting payload

--- OpenPGPForm/Main.php
+++ OpenPGPForm/Main.php
@@ -2,6 +2,28 @@
 
     namespace IdnoPlugins\OpenPGPForm {
         class Main extends \Idno\Common\Plugin {
+	    
+	    /**
+	     * Pass data through to gnupg (TODO: do this better)
+	     * @param type $data
+	     */
+	    private function decrypt($data) {
+		$gpg = \Idno\Core\site()->config()->opengpg_gnupg ? \Idno\Core\site()->config()->opengpg_gnupg : '/usr/bin/gpg';
+		
+		$command = "echo ". escapeshellarg($data) . " | $gpg --trust-model always --batch --yes -d";
+		
+		ob_start();
+		$return = "";
+		passthru($command, $return);
+		$result = ob_get_clean();
+		
+		if ($return == 0) {
+		    return $result;
+		}
+		
+		return false;
+	    }
+	    
             function registerPages() {
 		
 		
@@ -11,13 +33,25 @@
 		
 		
 		// If there's an encrypted payload, decrypt it and declare variables.
-                if (isset($_REQUEST['__encrypted_form_data'])) { // TODO: Do this properly!
-		    
-		    
-		    // TODO: Decrypt or error && unserialize && declare variables.
-		    
-		    
-                }
+		try {
+		    if (isset($_REQUEST['__encrypted_form_data'])) { // TODO: Do this properly!
+
+			$decrypted = $this->decrypt($_REQUEST['__encrypted_form_data']);
+			if (!$decrypted) throw new \Exception("There was a problem decrypting payload, check your logs and make sure the keyring is accessible to your web server user.");
+
+			parse_str($decrypted, $output);
+			if ((!$output) || (!is_array($output)))
+			    throw new Exception("Problem parsing decrypted payload.");
+
+			// Add the encrypted payload
+			foreach ($output as $key => $value) {
+			    \Idno\Core\site()->currentPage()->setInput($key, $value);
+			}
+
+		    }
+		} catch (\Exception $e) {
+		    \Idno\Core\site()->session()->addMessage($e->getMessage(), 'alert-danger');
+		}
             }
         }
     }
